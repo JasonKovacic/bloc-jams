2bf238e68b9aeaef5461c4fef3bc6ca5c49af99a
Jason <kovacic.jason@gmail.com>
2016-09-16 16:25:07 -0700

    Checkpoint #9 Commit #2

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,19 +1,16 @@
  var pointsArray = document.getElementsByClassName('point');
  
+ var revealPoint = function(point) {
+         point.style.opacity = 1;
+         point.style.transform = "scaleX(1) translateY(0)";
+         point.style.msTransform = "scaleX(1) translateY(0)";
+         point.style.WebkitTransform = "scaleX(1) translateY(0)";
+     }
+
  var animatePoints = function(points) {
  
-     var revealPoint = function(index) {
-         points[index].style.opacity = 1;
-         points[index].style.transform = "scaleX(1) translateY(0)";
-         points[index].style.msTransform = "scaleX(1) translateY(0)";
-         points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
-     }
+     forEach(points, revealPoint);
     
-     
-     for (var i = 0; i < points.length; i++) {
-         
-         revealPoint(i);
-     }
  };
 
 window.onload = function() {
@@ -23,7 +20,6 @@ window.onload = function() {
          animatePoints(pointsArray);
      }
     
-    var sellingPoints = document.getElementsByClassName('selling-points')[0];
 
     window.addEventListener('scroll', function(event) {
         if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
