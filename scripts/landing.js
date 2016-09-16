659437dcd4ee01366393e363fa336c2ce0ae58a8
Jason <kovacic.jason@gmail.com>
2016-09-16 11:03:38 -0700

    Checkpoint #9 Commit #1

---
+++ scripts/landing.js
@@ -0,0 +1,33 @@
+ var pointsArray = document.getElementsByClassName('point');
+ 
+ var animatePoints = function(points) {
+ 
+     var revealPoint = function(index) {
+         points[index].style.opacity = 1;
+         points[index].style.transform = "scaleX(1) translateY(0)";
+         points[index].style.msTransform = "scaleX(1) translateY(0)";
+         points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
+     }
+    
+     
+     for (var i = 0; i < points.length; i++) {
+         
+         revealPoint(i);
+     }
+ };
+
+window.onload = function() {
+    
+    
+     if (window.innerHeight > 950) {
+         animatePoints(pointsArray);
+     }
+    
+    var sellingPoints = document.getElementsByClassName('selling-points')[0];
+
+    window.addEventListener('scroll', function(event) {
+        if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+             animatePoints(pointsArray);   
+         }
+     });
+}
---
+++ scripts/utilities.js
@@ -0,0 +1,6 @@
+
+function forEach(array, callback) {
+    for(var i = 0; i < array.length; i++) {
+        callback(array[i]);
+        }
+    }
