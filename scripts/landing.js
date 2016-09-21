e974d5641b725cb1d6618a4caadffcaa8ba1df75
Jason <kovacic.jason@gmail.com>
2016-09-21 10:10:19 -0700

    Checkpoint #10 Commit

--- scripts/landing.js
+++ scripts/landing.js
@@ -15,15 +15,18 @@
 
 window.onload = function() {
     
-    
      if (window.innerHeight > 950) {
          animatePoints(pointsArray);
      }
     
+     var sellingPoints = document.getElementsByClassName('selling-points')[0];
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+    
 
     window.addEventListener('scroll', function(event) {
-        if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+       if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
              animatePoints(pointsArray);   
+         }
          }
      });
 }
