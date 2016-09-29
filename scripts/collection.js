e9a0b02bd500c6ed8da3334c082250e0c09e9b75
Jason <kovacic.jason@gmail.com>
2016-09-29 01:30:36 -0700

    Checkpoint #17 Commit

--- scripts/collection.js
+++ scripts/collection.js
@@ -1,5 +1,6 @@
- var collectionItemTemplate =
-     '<div class="collection-album-container column fourth">'
+ var buildCollectionItemTemplate = function() {
+     var template =
+    '<div class="collection-album-container column fourth">'
    + '  <img src="assets/images/album_covers/01.png"/>'
    + '  <div class="collection-album-info caption">'
    + '    <p>'
@@ -13,14 +14,16 @@
    + '  </div>'
    + '</div>'
  
-  window.onload = function() {
+       return $(template);
+ };
+ $(window).load(function() {
      // #1
-     var collectionContainer = document.getElementsByClassName('album-covers')[0];
+    var $collectionContainer = $('.album-covers');
      // #2
-     collectionContainer.innerHTML = '';
+    $collectionContainer.empty();
  
      // #3
-     for (var i = 0; i < 12; i++) {
-         collectionContainer.innerHTML += collectionItemTemplate;
-     }
- }
+    for (var i = 0; i < 12; i++) {
+    var $newThumbnail = buildCollectionItemTemplate();
+    $collectionContainer.append($newThumbnail);     }
+ });
